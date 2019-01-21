847f5de07ff551eb73c65f6ed0bcf43ff0c7c4b7
DavidMendelovits <David.Mendelovits@gmail.com>
2019-01-21 14:45:35 -0800

    first problem done

---
+++ d00/ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+# include <cstdlib>
+# include <string>
+
+char        *toUpper(char *s) {
+    std::string str = s;
+
+    for (std::string::size_type i = 0; i < str.length(); i++) {
+        s[i] = std::toupper(s[i]);
+    }
+    return (s);
+}
+
+int         main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    }
+    else {
+        for (int i = 1; i < argc; i++) {
+            std::cout<<toUpper(argv[i]);
+        }
+    std::cout<<"\n";
+    }
+
+    return (0);
+}
